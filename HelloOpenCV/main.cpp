d0d594f0e0cc7a216e52857f273bae468c8f8204
Csaba Trucza <csaba.trucza@gmail.com>
2015-12-22 16:20:58 +0200

    Initial Commit

---
+++ HelloOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloOpenCV
+//
+//  Created by Csaba Trucza on 22/12/15.
+//  Copyright © 2015 Csaba Trucza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
